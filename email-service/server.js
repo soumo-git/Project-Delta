9139163f7bb0d6bb0f8ac32607311085212c919d
soumo-git <soumom764@gmail.com>
2025-07-27 12:37:47 +0530

    Fix nodemailer function name

--- email-service/server.js
+++ email-service/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Create transporter using Gmail
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER || 'your-email@gmail.com',
